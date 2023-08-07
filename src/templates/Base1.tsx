2d4e131dd84b466173adfd969a3d63890bfa99bb
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-07 00:37:47 -0400

    Create Base1.tsx

---
+++ src/templates/Base1.tsx
@@ -0,0 +1,9 @@
+import { VerticalFeatures } from './VerticalFeatures';
+
+const Base1 = () => (
+  <div className="text-gray-600 antialiased">
+    <VerticalFeatures />
+  </div>
+);
+
+export { Base1 };
