7ee7c4ad521bb33ea7bbefa15d05b34f10686530
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-07 00:55:25 -0400

    Update VerticalFeatures1.tsx

--- src/templates/VerticalFeatures1.tsx
+++ src/templates/VerticalFeatures1.tsx
@@ -6,21 +6,8 @@ const VerticalFeatures1 = () => (
     <VerticalFeatureRow
       title="list1"
       description="picture"
-      image="https://garlicdelight.com/wp-content/uploads/20210319-reverse-shopping-list-768x768.png"
+      image="/assets/images/list.jpg"
       imageAlt="list"
-    />
-    <VerticalFeatureRow
-      title="鱿鱼"
-      description="鱿鱼筒，鱿鱼须"
-      image="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2008/6/12/0/squid.jpg.rend.hgtvcom.616.462.suffix/1383078078727.jpeg"
-      imageAlt="鱿鱼"
-      reverse
-    />
-    <VerticalFeatureRow
-      title="无骨牛肉"
-      description="Frozen Boneless Beef"
-      image="https://p.globalsources.com/IMAGES/PDT/B1181855913/frozen-beef-trimming-beef-trimming.jpg"
-      imageAlt="无骨牛肉"
     />
   </Section>
 );
