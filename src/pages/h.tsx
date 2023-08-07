a9e2dded0d03a206fd77cd787a69bedeeaf568f5
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-07 11:15:22 -0400

    Update h.tsx

--- src/pages/h.tsx
+++ src/pages/h.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 const H = () => (
-  <h1 className="title">Read <Link href="/posts/first-post">this page!</Link></h1>
+  <h1 className="title">Read <Link href="/posts/first-post">this page!</Link>
+  </h1>
 );
 export { H };
