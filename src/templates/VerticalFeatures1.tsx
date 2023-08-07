94431a158cec7986855091a499fd57e624855027
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-07 10:37:37 -0400

    Update VerticalFeatures1.tsx

--- src/templates/VerticalFeatures1.tsx
+++ src/templates/VerticalFeatures1.tsx
@@ -5,11 +5,12 @@ const VerticalFeatures1 = () => (
   <Section title="list" description="list">
     <VerticalFeatureRow
       title="list1"
-      description="picture"
-      image="/assets/images/list.jpg"
+      description="https://docs.google.com/spreadsheets/d/1cCG2CT8h4B10qXw7PMdM2VXs4kdV8rQqt-K3XkHApZs/edit?pli=1#gid=0"
+      image="/assets/images/northlinklp.jpg"
       imageAlt="list"
     />
   </Section>
+  <h1 className="title">Welcome to <a href="https://nextjs.org">Next.js!</a></h1>
 );
 
 export { VerticalFeatures1 };
