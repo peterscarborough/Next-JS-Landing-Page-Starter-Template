39ccb6b91c6a1df107a3c7bc7d6aa078cde1611b
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-06 23:52:12 -0400

    Update CTABanner.tsx

--- src/cta/CTABanner.tsx
+++ src/cta/CTABanner.tsx
@@ -10,6 +10,7 @@ const CTABanner = (props: ICTABannerProps) => (
   <div className="flex flex-col rounded-md bg-primary-100 p-4 text-center sm:flex-row sm:items-center sm:justify-between sm:p-12 sm:text-left">
     <div className="text-2xl font-semibold">
       <div className="text-gray-900">{props.title}</div>
+      <div className="text-gray-900">{props.title1}</div>
       <div className="text-primary-500">{props.subtitle}</div>
     </div>
 
