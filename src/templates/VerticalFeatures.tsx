98aa770393b3dccbdb2a0a6da91e2a2170cb252e
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-06 22:08:25 -0400

    Update VerticalFeatures.tsx

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -4,26 +4,26 @@ import { Section } from '../layout/Section';
 const VerticalFeatures = () => (
   <Section
     title="冻货"
-    description={"虾仁 P&D Shrimp\n鱿鱼筒\n鱿鱼须\n鱼柳\n有头中虾\n有头大虾\n去头白虾\n去头老虎虾\n中鸡翅\n牛孖筋\n猪脚\n无骨羊肩肉\n桂花鱼\n无头鳕鱼仔\n鱿鱼圈\n小春卷\n大春卷 \n无骨牛肉"}
+    description="  "
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="冻虾"
+      description="虾仁，有头虾，去头白虾，去头老虎虾"
       image="/assets/images/feature.svg"
-      imageAlt="First feature alt text"
+      imageAlt="冻虾"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="鱿鱼"
+      description="鱿鱼筒，鱿鱼须"
       image="/assets/images/feature2.svg"
-      imageAlt="Second feature alt text"
+      imageAlt="鱿鱼"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
+      title="无骨牛肉"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
+      image="/assets/images/wuguniurou.jpg"
+      imageAlt="无骨牛肉"
     />
   </Section>
 );
