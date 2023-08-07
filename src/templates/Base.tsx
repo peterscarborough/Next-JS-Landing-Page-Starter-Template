d3744359cb60e0fa7508b47e9846d2e862a7adb0
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-06 23:38:15 -0400

    Update Base.tsx

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -5,7 +5,6 @@ import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 
-
 const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
