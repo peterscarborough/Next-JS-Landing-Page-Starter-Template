9aec1df6a1721bbc825cdb4864b1af5b0ee25428
peterscarborough <phone.peter.toronto@gmail.com>
2023-08-07 00:17:48 -0400

    Update VerticalFeatures.tsx

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -6,19 +6,19 @@ const VerticalFeatures = () => (
     <VerticalFeatureRow
       title="冻虾"
       description="虾仁，有头虾，去头白虾，去头老虎虾"
-      image="/assets/images/feature.svg"
+      image="https://thekitchencommunity.org/wp-content/uploads/2020/12/Freezer-Burnt-Shrimp.jpg"
       imageAlt="冻虾"
     />
     <VerticalFeatureRow
       title="鱿鱼"
       description="鱿鱼筒，鱿鱼须"
-      image="/assets/images/feature2.svg"
+      image="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2008/6/12/0/squid.jpg.rend.hgtvcom.616.462.suffix/1383078078727.jpeg"
       imageAlt="鱿鱼"
       reverse
     />
     <VerticalFeatureRow
       title="无骨牛肉"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      description="Frozen Boneless Beef"
       image="https://p.globalsources.com/IMAGES/PDT/B1181855913/frozen-beef-trimming-beef-trimming.jpg"
       imageAlt="无骨牛肉"
     />
